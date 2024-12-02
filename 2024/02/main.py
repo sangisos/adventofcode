16d43b4c4d1aefe15b646f216b9a7b1c4aacf8db
sangisos <sangisos@gmail.com>
2024-12-02 12:40:03 +0100

    Day 2 first part

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -1,13 +1,18 @@
+from itertools import pairwise
 
 
 def main():
-	rawinput = None
-	with open("input_test") as f:
-		rawinput = f.readlines()
-	if rawinput == None:
-		return -1
-	
-	print(rawinput)
+	reports = []
+	with open("input.txt") as f:
+		for line in f:
+			reports.append([int(n) for n in line.split()])
+	safe = 0
+	for report in reports:
+		pw = []
+		pw.extend(pairwise(report))
+		if all([a<b<=a+3 for (a,b) in pw]) or all([a-3<=b<a for (a,b) in pw]):
+			safe += 1
+	print(safe)
 
 if __name__ == "__main__":
 	main()
