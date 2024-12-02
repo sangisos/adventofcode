9496ac2a25ddf1039e806bd776a43addded93e6e
htpc <htpc@HTPC-Vardagsrum>
2024-12-02 18:26:40 +0100

    Update template and vscode proj cwd

--- 2024/0/main.py
+++ 2024/0/main.py
@@ -1,13 +1,17 @@
 
 
 def main():
-	rawinput = None
-	with open("input_test") as f:
-		rawinput = f.readlines()
-	if rawinput == None:
+	lines = list()
+	
+	with open("input_test.txt") as f:
+	#with open("input.txt") as f:
+		for line in f:
+			lines.append([int(n) for n in line.split()])
+	if lines == None:
 		return -1
 	
-	print(rawinput)
+	print(lines)
 
 if __name__ == "__main__":
 	main()
+
