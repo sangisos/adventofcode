66aa07180118d3cf6ee98cb4ad697554d7f75559
htpc <htpc@HTPC-Vardagsrum>
2024-12-03 09:03:18 +0100

    Day 3 part 2, regex fail..

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -3,21 +3,37 @@ import re
 def main():
 	lines = list()
 	
-	with open("input_test.txt") as f:
+	#with open("input_test.txt") as f:
+	with open("input_test_2.txt") as f:
 	#with open("input.txt") as f:
 		for line in f:
 			lines.append(line)
 	if lines == None:
 		return -1
 	s=0
-	for line in lines:
-		r = re.findall(r'mul\([0-9]+,[0-9]+\)', line)
-		for m in r:
-			mul=[int(n) for n in m.strip("mul(").strip(")").split(",")]
-			s += mul[0]*mul[1]
-		
+	s2=0
+	line = ''.join(lines)
+	r = re.findall(r'mul\([0-9]+,[0-9]+\)', line)
+	#rem = ''.join(re.findall(r'(?:^|do\(\))(.*)(?:don\'t\(\)|$)', line))
+	#r2 = re.findall(r'mul\([0-9]+,[0-9]+\)', rem)
+	#mulordont = re.findall(r'(?:don\'t\(\).*?do\(\))|(mul\([0-9]+,[0-9]+\))', line)
+	#print(mulordont)
+	#r2 = list(filter(None, mulordont))
+	donts = line.split('don\'t()')
+	ccat=donts[0]
+	for p in donts[1:]:
+		ccat += p[p.find('do()'):]
+	r2 = re.findall(r'mul\([0-9]+,[0-9]+\)', ccat)
+	#print(donts)
+	#r2 = re.findall(r'(?^|do\(\)).*mul\([0-9]+,[0-9]+\)', line)
+	for m in r:
+		mul=[int(n) for n in m.strip("mul(").strip(")").split(",")]
+		s += mul[0]*mul[1]
+	for m in r2:
+		mul=[int(n) for n in m.strip("mul(").strip(")").split(",")]
+		s2 += mul[0]*mul[1]
 
-	print(lines, r, s)
+	print( r2, s, s2)
 
 if __name__ == "__main__":
 	main()
