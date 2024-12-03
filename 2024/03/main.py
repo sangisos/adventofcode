56a8652ca3c56a6237bfc9b39280e5929ff4f677
htpc <htpc@HTPC-Vardagsrum>
2024-12-03 09:59:00 +0100

    Day 3 part 2 solved with regex

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -4,21 +4,22 @@ def main():
 	lines = list()
 	
 	#with open("input_test.txt") as f:
-	with open("input_test_2.txt") as f:
-	#with open("input.txt") as f:
+	#with open("input_test_2.txt") as f:
+	with open("input.txt") as f:
 		for line in f:
-			lines.append(line)
+			lines.append(line.rstrip())
 	if lines == None:
 		return -1
 	s=0
 	s2=0
+	s3=0
 	line = ''.join(lines)
 	r = re.findall(r'mul\([0-9]+,[0-9]+\)', line)
 	#rem = ''.join(re.findall(r'(?:^|do\(\))(.*)(?:don\'t\(\)|$)', line))
 	#r2 = re.findall(r'mul\([0-9]+,[0-9]+\)', rem)
-	#mulordont = re.findall(r'(?:don\'t\(\).*?do\(\))|(mul\([0-9]+,[0-9]+\))', line)
+	mulordont = re.findall(r'(?:don\'t\(\).*?do\(\))|(mul\([0-9]+,[0-9]+\))', line)
 	#print(mulordont)
-	#r2 = list(filter(None, mulordont))
+	r2test = list(filter(None, mulordont))
 	donts = line.split('don\'t()')
 	ccat=donts[0]
 	for p in donts[1:]:
@@ -33,8 +34,14 @@ def main():
 		mul=[int(n) for n in m.strip("mul(").strip(")").split(",")]
 		s2 += mul[0]*mul[1]
 
-	print( r2, s, s2)
+	for m in r2test:
+		mul=[int(n) for n in m.strip("mul(").strip(")").split(",")]
+		s3 += mul[0]*mul[1]
 
+	print( r2, s, s2, s3)
+	for l in zip(r2, r2test):
+		if l[0] != l[1]:
+			print(l)
 if __name__ == "__main__":
 	main()
 
