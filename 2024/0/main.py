165f913e7a626704188f4f3f6cea2922974c1be8
htpc <htpc@HTPC-Vardagsrum>
2024-12-04 13:07:19 +0100

    Update template main and input_test

--- 2024/0/main.py
+++ 2024/0/main.py
@@ -6,10 +6,13 @@ def main():
 	with open("input_test_1.txt") as f:
 	#with open("input.txt") as f:
 		for line in f:
-			lines.append([int(n) for n in line.split()])
+			print(line.split())
+			lines.append([int(n) for n in line.rstrip().split()])
 	
-	sum1 = 0
-	sum2 = 0
+	print(lines)
+
+	sum1 = sum(lines[0])
+	sum2 = sum(lines[1])
 	
 	print("Part 1:", sum1, "Part 2:", sum2)
 
